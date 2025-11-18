710a44d99e865c023f43d656e267485027e2da34
Yam Sol Britto Bertamini <yam.sol@inatel.br>
2025-11-18 20:18:13 -0300

    refac: atualizando rotas /cards/**

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -6,6 +6,7 @@ const router = Router();
 // Auth routes
 router.post('/login', userController.login.bind(userController));
 router.post('/logout', userController.logout.bind(userController));
+
 router.post('/register', userController.register.bind(userController));
 
 export { router as authRoutes };
